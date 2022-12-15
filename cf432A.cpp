e29b6a157cb1a578d1397f0290da27331e80982f
Umayer <umayercse16@gmail.com>
2022-12-15 22:04:50 +0600

    ChoosingTeams

---
+++ cf432A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,k,x;
+    cin>>n>>k;
+    vector<int> v;
+    for(int i=0;i<n;i++)
+    {
+        cin>>x;
+        v.push_back(x);
+    }
+    sort(v.begin(),v.end());
+    int cont=0;
+    for(int i=0;i<n;i++){
+        v[i]=v[i]+k;
+        if(v[i]<=5)
+            cont++;
+    }
+    cout<<cont/3<<endl;
+}
